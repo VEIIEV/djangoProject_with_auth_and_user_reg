2b7dacb8aef365afc92a18b38596c140669cb473
veiiev <worldloflwarcraft@mail.ru>
2023-06-14 01:11:55 +0400

    реализавал возможность сброса и смены пороля средствами django.contrib.auth

--- account/views.py
+++ account/views.py
@@ -1,30 +1,34 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from account.forms import LoginFrom
 
 
 # Create your views here.
 
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginFrom(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             # auth возвращает объект User если учетные данные правильные
-#             user = authenticate(request,
-#                                 username=cd['username'],
-#                                 password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     # login задает пользователя в текущем сеансе
-#                     login(request, user)
-#                     return HttpResponse('Authenticated successfully')
-#                 else:
-#                     return HttpResponse('Disabled account')
-#             else:
-#                 return HttpResponse('Invalid login')
-#     else:
-#         form = LoginFrom()
-#     return render(request, 'account/login.html', {'form': form})
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginFrom(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # auth возвращает объект User если учетные данные правильные
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    # login задает пользователя в текущем сеансе
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginFrom()
+    return render(request, 'account/login.html', {'form': form})
 
 
 @login_required
--- djangoProject_with_auth_and_user_reg/settings.py
+++ djangoProject_with_auth_and_user_reg/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import django.core.mail.backends.console
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,11 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+# параметры, которые позволяют django вызывать эти url по умолчанию для полей с необходимой аутентификацией
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+# параметр для написания почтовых писем в консоль, полезно для тестов
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Application definition
 
@@ -76,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject_with_auth_and_user_reg.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -86,7 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -106,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -117,7 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
